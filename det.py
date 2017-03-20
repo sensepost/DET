9af7fbc8024bc05e4a11f489576f0f534986a4eb
Yassine Tioual <yassine.tioual@gmail.com>
2017-03-20 15:48:10 +0100

    Handling missing packets

--- det.py
+++ det.py
@@ -181,6 +181,7 @@ class Exfiltration(object):
             files[jobid]['filename'] = message[1].lower()
             files[jobid]['data'] = []
             files[jobid]['packets_number'] = []
+            files[jobid]['packets_len'] = -1
             warning("Register packet for file %s with checksum %s" %
                     (files[jobid]['filename'], files[jobid]['checksum']))
 
@@ -219,13 +220,21 @@ class Exfiltration(object):
                     self.register_file(message)
                 # done packet
                 elif (message[2] == "DONE"):
-                    self.retrieve_file(jobid)
+                    files[jobid]['packets_len'] = int(message[1])
+                    #Check if all packets have arrived
+                    if files[jobid]['packets_len'] == len(files[jobid]['data']):
+                        self.retrieve_file(jobid)
+                    else:
+                        warning("[!] Received the last packet, but some are still missing. Waiting for the rest...")
                 # data packet
                 else:
                     # making sure there's a jobid for this file
                     if (jobid in files and message[1] not in files[jobid]['packets_number']):
                         files[jobid]['data'].append(''.join(message[2:]))
                         files[jobid]['packets_number'].append(int(message[1]))
+                        #In case this packet was the last missing one
+                        if files[jobid]['packets_len'] == len(files[jobid]['data']):
+                            self.retrieve_file(jobid)
         except:
             raise
             pass
