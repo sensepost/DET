27d694afd70f0afcaf5d8082bfa89b02f34ea221
Yassine Tioual <yassine.tioual@gmail.com>
2017-10-09 18:44:07 +0200

    Update det.py
    - Restore credit

--- det.py
+++ det.py
@@ -326,7 +326,7 @@ def main():
     global threads, config
 
     parser = argparse.ArgumentParser(
-        description='Data Exfiltration Toolkit (Conix-Security)')
+        description='Data Exfiltration Toolkit (SensePost)')
     parser.add_argument('-c', action="store", dest="config", default=None,
                         help="Configuration file (eg. '-c ./config-sample.json')")
     parser.add_argument('-f', action="append", dest="file",
